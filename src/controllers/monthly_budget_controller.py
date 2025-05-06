1517f28dd04c302139d6953cc4ae9fea737812e9
Sam Alexis Huertas <156677818+SamHuertas@users.noreply.github.com>
2025-05-06 21:36:06 +0800

    added monthly budget model

--- src/controllers/monthly_budget_controller.py
+++ src/controllers/monthly_budget_controller.py
@@ -1,22 +1,21 @@
 import sys
-from datetime import datetime
 from PyQt6 import QtWidgets, QtCore
-from PyQt6.QtWidgets import QTableWidgetItem
+from PyQt6.QtCore import Qt, QDate
+from PyQt6.QtWidgets import QTableWidget, QTableWidgetItem, QMessageBox
 from src.database.db_manager import DBManager
+from src.models.monthly_budget_model import MonthlyBudgetModel
 
 class MonthlyBudgetController:
-    def __init__(self, main_window):
+    def __init__(self, main_window, model: MonthlyBudgetModel):
         self.main_window = main_window
+        self.model = model
         self.db = DBManager()
         self.setup_ui_connections()
         self.setup_table()
+        self.setup_calendar()
 
     def setup_ui_connections(self):
         self.main_window.AddBudgetButton.clicked.connect(self.add_budget)
-        self.main_window.BudgetDateEdit.setDisplayFormat("MMMM yyyy")
-        current_date = QtCore.QDate.currentDate()
-        self.main_window.BudgetDateEdit.setDate(current_date)
-        self.main_window.BudgetDateEdit.setCalendarPopup(True)
 
 
     def setup_table(self):
@@ -34,24 +33,9 @@ class MonthlyBudgetController:
         table.setEditTriggers(QtWidgets.QAbstractItemView.EditTrigger.NoEditTriggers)
         table.setAlternatingRowColors(True)
 
-    def add_budget(self):
-        budget_amount_text = self.main_window.BudgetInput.text()
-
-        if not budget_amount_text:
-            QtWidgets.QMessageBox.warning(self.main_window, "Input Error", "Please enter a budget amount.")
-            return
-        try:
-            budget_amount = float(budget_amount_text)
-            if budget_amount <= 0:
-                raise ValueError("Budget amount must be greater than zero.")
-        except ValueError:
-            QtWidgets.QMessageBox.warning(self.main_window, "Input Error", "Please enter a valid budget amount.")
-            return
+    def setup_calendar(self):
+        self.main_window.BudgetDateEdit.setCalendarPopup(False)
+        self.main_window.BudgetDateEdit.setDate(QtCore.QDate.currentDate())
+        self.main_window.BudgetDateEdit.setDisplayFormat("MMMM yyyy")
         
-        date = self.main_window.BudgetDateEdit.date()
-        month = date.month()
-        year = date.year()  
-
-        self.db.execute("INSERT INTO MonthlyBudget (amount, month, year) VALUES (%s, %s, %s)", (budget_amount_text, month, year),)
-        print("Budget added to the database.")
-
+        
